0139ad372f0deaffa29da68abfafefc01d47ab0d
Roman Lebedenko <ceobit90@gmail.com>
2021-05-18 11:35:53 +0200

    fixed an api problem with getFilmbyId

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-import {getMovieCredits, getRatedFilms} from './api.js';
-
+import { getMovieCredits, getRatedFilms, getFilmById } from './api.js';
 
 // getRatedFilms(1).then(arr => console.log(arr));
 // getMovieCredits().then(data => console.log(data));
+
+// getFilmById('101').then(data => console.log(data));
