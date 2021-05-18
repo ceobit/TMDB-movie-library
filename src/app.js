6535c11ac8bea776a305c5350abaf28f43cf9146
Roman Lebedenko <ceobit90@gmail.com>
2021-05-18 10:13:44 +0200

    API example

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
-import {getMovieCredits, getRatedFilms} from './api.js';
-
+import { getMovieCredits, getRatedFilms, getFilm } from './api.js';
 
 // getRatedFilms(1).then(arr => console.log(arr));
-// getMovieCredits().then(data => console.log(data));
+// getMovieCredits("123").then((data) => console.log(data));
+
+//
+// const poster = document.querySelector('.poster');
+// const title = document.querySelector('.film_title');
+//
+//
+// const handleTitle = (e) => {
+//
+//   const filmId = e.target.parentElement.getAttribute('data-filmId');
+//
+//   getMovieCredits(filmId).then((data) => title.textContent = data.title);
+//
+// };
+// poster.addEventListener('click', handleTitle);
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const API_key = '5fff6fd33738e50068fa6d70b3ad22c2';
 export const baseURL= 'https://api.themoviedb.org/3/';
-export const posterURL = 'https://image.tmdb.org/t/p/w500/';
+export const posterURL = 'https://image.tmdb.org/t/p/w500';
