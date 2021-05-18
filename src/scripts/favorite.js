7f6c45ee7981cfc7779c2d03d89d6001214c16c3
MaryamPayenda <maryampayanda12@gmail.com>
2021-05-18 15:38:23 +0200

    added

---
+++ src/scripts/favorite.js
@@ -0,0 +1,27 @@
+// delete btn
+const ul = document.querySelector('#movieDesc');
+ul.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const button = event.target;
+    const li = button.parentNode;
+    const ul = li.parentNode;
+    if (button.textContent === 'Remove') {
+      ul.removeChild(li);
+    }
+  }
+});
+
+// search
+const searchBar = document.forms['search-book'].querySelector('input');
+searchBar.addEventListener('keyup', function (e) {
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName('li');
+  Array.form(books).forEach(function (book) {
+    const title = book.firstElementChild.textContent;
+    if (title.toLowerCase().indexOf(term) != -1) {
+      book.style.display = 'block';
+    } else {
+      book.style.display = 'none';
+    }
+  });
+});
