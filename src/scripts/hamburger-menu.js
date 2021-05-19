e7d6c2149d5ba156aa6b21258625521e50048585
viviane <v.buschwallace@yahoo.com>
2021-05-19 21:36:19 +0200

    hamburger menu animation done

--- src/scripts/hamburger-menu.js
+++ src/scripts/hamburger-menu.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector('.hamburger-container');
+const hamburger = document.querySelector('.hamburger-menu');
 const hamburgerMenuList = document.querySelector('.navbar-pagelinks ul');
 hamburger.addEventListener('click', showHam);
 
@@ -6,9 +6,11 @@ let clickedHamburger = false;
 
 function showHam() {
   if (clickedHamburger == false) {
+    hamburger.style.animation = '1s 1 normal rotate';
     hamburgerMenuList.style.display = 'flex';
     clickedHamburger = true;
-  } else {
+  } else if (clickedHamburger == true) {
+    hamburger.style.animation = '1s 1 normal rotateBack';
     hamburgerMenuList.style.display = 'none';
     clickedHamburger = false;
   }
