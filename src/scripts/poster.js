fc0e044a8b18e44cff1c4895e7e9e11e0304bb40
viviane <v.buschwallace@yahoo.com>
2021-05-19 09:55:42 +0200

    transform on hover changes

--- src/scripts/poster.js
+++ src/scripts/poster.js
@@ -1,1 +1,6 @@
-const watched = document.querySelector('');
+// set to 'watched' or 'not watched'
+
+const watched = document.querySelector('.watched');
+console.log(watched);
+console.log(watched.classList);
+if (watched.classList.contains('not-watched')) console.log(watched);
